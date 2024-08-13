e149b4ad29c0686dc1f7ea3f7237619f1d69cb74
Rohit Saluja <saluja730@gmail.com>
2024-08-13 08:42:51 +0530

    updated the user image of the header is done

---
+++ app/components/side-bar.tsx
@@ -0,0 +1,3 @@
+export default function SideBar() {
+  return <div className="fixed min-w-[200px] min-h-screen left-0 border-r border"></div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/header";
+import SideBar from "./components/side-bar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <SideBar />
     </div>
   );
 }

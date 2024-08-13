d22acac8b8bd4f5e1c9ee88e0a8662ff5b57165c
Rohit Saluja <saluja730@gmail.com>
2024-08-13 09:35:36 +0530

    added the buttons of table on the employees table

--- app/(admin)/employees/page.tsx
+++ app/(admin)/employees/page.tsx
@@ -1,4 +1,7 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { MdModeEditOutline, MdOutlineDelete } from "react-icons/md";
 
 export default function Employees() {
   return (
@@ -14,6 +17,41 @@ export default function Employees() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold">Employees</h1>
+          <h2 className="text-sm">Manage employees</h2>
+        </div>
+        <Button variant={"default"}>Add New</Button>
+      </div>
+      <Table>
+        <TableCaption>A list of your employees.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[160px]">Employee Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Address</TableHead>
+            <TableHead>Phone Number</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">
+              <Button variant="outline" size="icon">
+                <MdModeEditOutline className="text-2xl" />
+              </Button>
+              <Button variant="outline" size="icon">
+                <MdOutlineDelete className="text-2xl" />
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }

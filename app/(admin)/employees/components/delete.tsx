927f8005edb4e71e1fdb06766eaa2df5c4087ad3
Rohit Saluja <saluja730@gmail.com>
2024-08-13 11:43:18 +0530

    added the delete confirmatin popuup for the emplooee is done

--- app/(admin)/employees/components/delete.tsx
+++ app/(admin)/employees/components/delete.tsx
@@ -1,47 +1,22 @@
 "use client";
-import { z } from "zod";
-import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/components/ui/use-toast";
 import { useState } from "react";
-import { useAppDispatch } from "@/app/store/hooks";
-import { updateEmployee } from "@/app/store/features/employee-slice";
+import { deleteEmployee } from "@/app/store/features/employee-slice";
 import { Employee } from "@/app/types/employee";
-import { MdModeEditOutline } from "react-icons/md";
+import { MdOutlineDelete } from "react-icons/md";
+import { useAppDispatch } from "@/app/store/hooks";
+import { Button } from "@/components/ui/button";
 
 export default function Edit({ employee }: { employee: Employee }) {
   const { toast } = useToast();
-  const [open, setOpen] = useState(false);
   const dispatch = useAppDispatch();
-
-  const formSchema = z.object({
-    name: z.string().min(1, { message: "Name is required" }),
-    email: z.string().email().min(1, { message: "Email is required" }),
-    phone: z.string().min(1, { message: "Phone is required" }),
-    address: z.string().min(1, { message: "Address is required" }),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: employee.name,
-      email: employee.email,
-      phone: employee.phone,
-      address: employee.address,
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    dispatch(updateEmployee(values));
+  const [open, setOpen] = useState(false);
+  function onSubmit() {
+    dispatch(deleteEmployee(employee));
     toast({
-      description: "Employee is updated",
+      description: "Employee is deleted",
     });
-    form.reset();
     setOpen(false);
   }
 
@@ -49,70 +24,24 @@ export default function Edit({ employee }: { employee: Employee }) {
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
         <Button variant="outline" size="icon">
-          <MdModeEditOutline className="text-2xl" />
+          <MdOutlineDelete className="text-2xl" />
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Add Employees</DialogTitle>
+          <DialogTitle>Delete Employees</DialogTitle>
           <DialogDescription>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Email" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Phone</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Phone" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="address"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Address</FormLabel>
-                      <FormControl>
-                        <Textarea placeholder="Address" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Submit</Button>
-              </form>
-            </Form>
+            <div className="flex flex-col gap-4">
+              <h1>Are you sure you want to delete this employee?</h1>
+              <div className="flex justify-end items-center gap-2">
+                <Button variant={"outline"} type="button" onClick={() => setOpen(false)}>
+                  Cancel
+                </Button>
+                <Button variant={"destructive"} onClick={() => onSubmit()}>
+                  Delete
+                </Button>
+              </div>
+            </div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>

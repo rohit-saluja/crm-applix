b48edb5548c2ad7c6421da665c3e3ecc8b1c940e
Rohit Saluja <saluja730@gmail.com>
2024-08-18 12:00:13 +0530

    updated the post request is done

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -2,6 +2,21 @@ import { type NextRequest } from "next/server";
 import prisma from "@/prisma";
 
 export async function GET(request: NextRequest) {
-  const employees = prisma.employee.findMany({});
+  const employees = await prisma.employee.findMany({});
   return Response.json(employees);
+}
+
+export async function Post(request: NextRequest) {
+  console.log(request.formData);
+  console.log(request.body);
+
+  await prisma.employee.create({
+    data: {
+      name: "rohit",
+      address: "ajsdfjl",
+      email: "lajsdflj",
+      phone: "jlasjdf",
+    },
+  });
+  return Response.json(true);
 }
